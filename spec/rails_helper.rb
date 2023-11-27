f2d89901d1d55e1aed81a172136931b06d3bb7f3
Tori Enyart <torienyart@gmail.com>
2023-11-27 13:05:51 -0500

    Make bug fixes for merchant model spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,6 @@ RSpec.configure do |config|
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
-
       with.library :rails
     end
   end
